b87dcb9c2a27ace7ca94602a9b675a74761add5a
可知 <1538001851@qq.com>
2020-11-24 16:43:00 +0800

    修复了计算距离和中心点时的一些问题，添加了dbi指数的计算

--- k_means.py
+++ k_means.py
@@ -26,7 +26,10 @@ def get_dire(filepath):
     return list(map(list, zip(*result)))
 
 # 计算欧氏距离
-def distance(point1,point2,dim):
+def distance(point1,point2):
+    dim=len(point1)
+    if dim != len(point2):
+        print('error! dim of point1 and point2 is not same')
     dist=0
     for i in range(dim):
         dist+=(point1[i]-point2[i])*(point1[i]-point2[i])
@@ -39,14 +42,14 @@ def get_category(dire,k,k_center):
     for i in range(shape[0]):
         Min=1
         for j in range(k):
-            dist=distance(dire[i],k_center[j],shape[1])
+            dist=distance(dire[i],k_center[j])
             if dist<Min:
                 Min=dist
                 MinNum=j
         k_categories[MinNum].append(i)
     return k_categories
 
-def show_k_meas(k_result):
+def show_k_means(k_result):
     k,k_categories,dire=k_result['k'],k_result['k_categories'],k_result['dire']
     for i in range(k):
         x,y,z=[],[],[]
@@ -86,12 +89,45 @@ def k_means(dire,k):
                 temp=0
                 for w in k_categories[j]:
                     temp+=dire[w][i]
-                k_center_new[j][i]=temp/len(k_categories[j])
+                if len(k_categories[j])!=0:
+                    k_center_new[j][i]=temp/len(k_categories[j])
     return {'k_center':k_center,'k_categories':k_categories,'dire':dire,'k':k}
+
+def dbi(k_result):
+    k_center,k_categories,dire,k=k_result['k_center'],k_result['k_categories'],k_result['dire'],k_result['k']
+    # 簇内平均距离
+    k_ave_dist=[0 for index in range(k)]
+    for i in range(k):
+        temp=0
+        for item in k_categories[i]:
+            temp+=distance(k_center[i],dire[item])
+        k_ave_dist[i]=temp/len(k_categories[i])
+    # 簇中心之间距离
+    k_center_dist=[[0 for row in range(k)] for col in range(k)]
+    for i in range(k):
+        for j in range(k):
+            k_center_dist[i][j]=distance(k_center[i],k_center[j])
+    # 计算dbi
+    DB=0
+    for i in range(k):
+        Max=0
+        for j in range(k):
+            if i !=j:
+                temp=(k_ave_dist[i]+k_ave_dist[j])/k_center_dist[i][j]
+                if temp>Max:
+                    Max=temp
+        DB+=Max
+    return DB/k
 
 
 
 if __name__ == "__main__":
     data=get_dire('data_processed.csv')
-    k_result=k_means(data,2)
-    show_k_meas(k_result)
+    # show_k_means(k_means(data,10))
+    DBI=[]
+    for i in range(2,50):
+        k_result=k_means(data,i)
+        DBI.append(dbi(k_result))
+        print(DBI[-1])
+    plt.plot(DBI)
+    plt.show()
